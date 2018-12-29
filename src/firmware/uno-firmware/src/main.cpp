afae3e0477d79cfd7ae51be83b37b6bd882f001f
Steven Burt <sjburt@gmail.com>
2018-12-28 22:07:08 -0800

    Spit IMU messages

--- src/firmware/uno-firmware/src/main.cpp
+++ src/firmware/uno-firmware/src/main.cpp
@@ -1,32 +1,43 @@
-// Test code for Adafruit GPS modules using MTK3329/MTK3339 driver
-//
-// This code shows how to listen to the GPS module in an interrupt
-// which allows the program to have more 'freedom' - just parse
-// when a new NMEA sentence is available! Then access data when
-// desired.
-//
-// Tested and works great with the Adafruit Ultimate GPS module
-// using MTK33x9 chipset
-//    ------> http://www.adafruit.com/products/746
-// Pick one up today at the Adafruit electronics shop
-// and help support open source hardware & software! -ada
+
+#if (ARDUINO >= 100)
+ #include <Arduino.h>
+#else
+ #include <WProgram.h>
+#endif
 
 #include <ctime>
-#include <cmath>
 #include <Adafruit_GPS.h>
+#include <Wire.h>
+#include <Adafruit_Sensor.h>
+#include <Adafruit_BNO055.h>
+#include <utility/imumaths.h>
+#include <stdio.h>
+
 #include <ros.h>
 
 #include <std_msgs/Int32.h>
 #include <sensor_msgs/NavSatFix.h>
+#include <sensor_msgs/Imu.h>
+#include <sensor_msgs/MagneticField.h>
+
+#include <cmath>
+
 
 HardwareSerial mySerial = HardwareSerial();
 Adafruit_GPS GPS(&mySerial);
+Adafruit_BNO055 bno = Adafruit_BNO055(55);
+
 ros::NodeHandle nh;
 
 std_msgs::Int32 num_sats_msg;
 sensor_msgs::NavSatFix nav_msg;
+sensor_msgs::Imu imu_msg;
+sensor_msgs::MagneticField mag_msg;
+
+ros::Publisher imu_pub("imu", &imu_msg);
 ros::Publisher nav("gps", &nav_msg);
 ros::Publisher num_sats("num_sats", &num_sats_msg);
+ros::Publisher mag("mag", &mag_msg);
 
 void setup()
 {
@@ -34,21 +45,33 @@ void setup()
   nh.initNode();
   nh.advertise(nav);
   nh.advertise(num_sats);
-  delay(2000);
-  // 9600 NMEA is the default baud rate for Adafruit MTK GPS's- some use 4800
+  nh.advertise(imu_pub);
+  nh.advertise(mag);
 
   GPS.begin(9600);
-
   GPS.sendCommand(PMTK_SET_NMEA_OUTPUT_RMCGGA);
-  // Set the update rate
   GPS.sendCommand(PMTK_SET_NMEA_UPDATE_1HZ);   // 1 Hz update rate
-  // GPS.sendCommand(PGCMD_ANTENNA);
+
+  imu_msg.header.seq = 0;
+  imu_msg.header.frame_id = "imu_frame";
+  imu_msg.orientation_covariance[0] = -1;
+  imu_msg.angular_velocity_covariance[0] = -1;
+  imu_msg.linear_acceleration_covariance[0] = -1;
+
+  mag_msg.header.seq = 0;
+  mag_msg.header.frame_id = "imu_frame";
+  memset(&mag_msg.magnetic_field_covariance, 0, sizeof(mag_msg.magnetic_field_covariance));
+
+
+  bno.begin();
+  bno.setMode(Adafruit_BNO055::OPERATION_MODE_NDOF);
+  bno.setAxisSign(Adafruit_BNO055::REMAP_SIGN_P5);
 
   delay(1000);
   // Ask for firmware version
   mySerial.println(PMTK_Q_RELEASE);
   num_sats_msg.data = 0;
-  nav_msg.header.frame_id = "my_frame";
+  nav_msg.header.frame_id = "gps_frame";
   nav_msg.header.seq = 0;
   nav_msg.status.status = -1;
   nav_msg.status.service = 1;
@@ -70,6 +93,7 @@ void loop()                     // run over and over again
   if (GPS.newNMEAreceived()) {
     if (!GPS.parse(GPS.lastNMEA()))   // this also sets the newNMEAreceived() flag to false
       return;  // we can fail to parse a sentence in which case we should just wait for another
+
     struct tm timeinfo;
     timeinfo.tm_year = GPS.year + 100;
     timeinfo.tm_mon = GPS.month - 1;
@@ -99,6 +123,36 @@ void loop()                     // run over and over again
     nav.publish(&nav_msg);
     num_sats.publish(&num_sats_msg);
   }
+
+  imu::Quaternion quat = bno.getQuat();
+  quat.normalize();
+
+  imu_msg.orientation.w = quat.w();
+  imu_msg.orientation.x = quat.x();
+  imu_msg.orientation.y = quat.y();
+  imu_msg.orientation.z = quat.z();
+
+  auto gyro = bno.getVector(Adafruit_BNO055::VECTOR_GYROSCOPE);
+  imu_msg.angular_velocity.x = gyro[0];
+  imu_msg.angular_velocity.y = gyro[1];
+  imu_msg.angular_velocity.z = gyro[2];
+
+  auto accel = bno.getVector(Adafruit_BNO055::VECTOR_ACCELEROMETER);
+  imu_msg.linear_acceleration.x = accel[0];
+  imu_msg.linear_acceleration.y = accel[1];
+  imu_msg.linear_acceleration.z = accel[2];
+
+  imu_msg.header.stamp = nh.now();
+  imu_msg.header.seq++;
+  imu_pub.publish(&imu_msg);
+
+  auto mag_field = bno.getVector(Adafruit_BNO055::VECTOR_MAGNETOMETER);
+  mag_msg.magnetic_field.x = mag_field[0] / 1000000;
+  mag_msg.magnetic_field.y = mag_field[1] / 1000000;
+  mag_msg.magnetic_field.z = mag_field[2] / 1000000;
+  mag_msg.header.stamp = nh.now();
+  mag_msg.header.seq++;
+  mag.publish(&mag_msg);
 
   nh.spinOnce();
 }
